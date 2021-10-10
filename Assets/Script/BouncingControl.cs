2c38a64e2cd803fb779b5ef80bdef95a3482f60f
NaufalAmajid <naufalamajid@gmail.com>
2021-10-10 19:08:20 +0700

    second commit

---
+++ Assets/Script/BouncingControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncingControl : MonoBehaviour
+{
+
+    private Rigidbody2D rigidbody2D;
+
+    public float x;
+    public float y;
+
+    void Start()
+    {
+        
+        rigidbody2D = GetComponent<Rigidbody2D>();
+
+        Invoke("Bouncing", 0);
+
+    }
+
+    void Bouncing(){
+
+        float RandomDirection = Random.Range(0, 2);
+
+        if (RandomDirection < 1.0f)
+        {
+            rigidbody2D.AddForce(new Vector2(-x, y));
+        
+        }else{
+            
+            rigidbody2D.AddForce(new Vector2(x, y));
+
+        }
+
+    }
+
+}
