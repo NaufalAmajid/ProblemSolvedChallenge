5c1c15232a50388b89955f8ced594ee1e03e4efc
NaufalAmajid <naufalamajid@gmail.com>
2021-10-10 22:09:31 +0700

    fourth commit

---
+++ Assets/Script/MovementControl.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementControl : MonoBehaviour
+{   
+    public Rigidbody2D rigidbody2D;
+
+    public float speed = 10.0f;
+
+    public KeyCode upButton = KeyCode.UpArrow;
+    public KeyCode downButton = KeyCode.DownArrow;
+    public KeyCode rightButton = KeyCode.RightArrow;
+    public KeyCode leftButton = KeyCode.LeftArrow;
+
+    public float x = 9.0f;
+
+    public float y = 9.0f;
+    
+    // Update is called once per frame
+    void Update()
+    {   
+        Vector2 velocity = rigidbody2D.velocity;
+
+        if(Input.GetKey(upButton))
+        {
+
+            velocity.y = speed;
+            
+        }else if(Input.GetKey(downButton)){
+            
+            velocity.y = -speed;
+
+        }else if(Input.GetKey(rightButton)){
+            
+            velocity.x = speed;
+
+        }else if(Input.GetKey(leftButton)){
+            
+            velocity.x = -speed;
+
+        }else{
+            
+            velocity.y = 0.0f;
+
+            velocity.x = 0.0f;
+
+        }
+        
+        rigidbody2D.velocity = velocity;
+
+    }
+
+}
